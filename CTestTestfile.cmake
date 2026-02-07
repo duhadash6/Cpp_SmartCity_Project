d6fb83d8b887c754023ad733e7b1362759e78988
dohaziouani <dohaziouani3@gmail.com>
2026-02-07 03:43:38 +0100

    Add files via upload

---
+++ CTestTestfile.cmake
@@ -0,0 +1,16 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/takie/Downloads/Projet_Cpp/Projet-Cpp-Sous-Projet-3-Routage Dynamique/Sous_Projet_3_Routage_Dynamique_Cpp
+# Build directory: C:/Users/takie/Downloads/Projet_Cpp/Projet-Cpp-Sous-Projet-3-Routage Dynamique/Sous_Projet_3_Routage_Dynamique_Cpp/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(EventTest "C:/Users/takie/Downloads/Projet_Cpp/Projet-Cpp-Sous-Projet-3-Routage Dynamique/Sous_Projet_3_Routage_Dynamique_Cpp/build/test_Event.exe")
+set_tests_properties(EventTest PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/takie/Downloads/Projet_Cpp/Projet-Cpp-Sous-Projet-3-Routage Dynamique/Sous_Projet_3_Routage_Dynamique_Cpp/CMakeLists.txt;212;add_test;C:/Users/takie/Downloads/Projet_Cpp/Projet-Cpp-Sous-Projet-3-Routage Dynamique/Sous_Projet_3_Routage_Dynamique_Cpp/CMakeLists.txt;0;")
+add_test(GraphTest "C:/Users/takie/Downloads/Projet_Cpp/Projet-Cpp-Sous-Projet-3-Routage Dynamique/Sous_Projet_3_Routage_Dynamique_Cpp/build/test_Graph.exe")
+set_tests_properties(GraphTest PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/takie/Downloads/Projet_Cpp/Projet-Cpp-Sous-Projet-3-Routage Dynamique/Sous_Projet_3_Routage_Dynamique_Cpp/CMakeLists.txt;213;add_test;C:/Users/takie/Downloads/Projet_Cpp/Projet-Cpp-Sous-Projet-3-Routage Dynamique/Sous_Projet_3_Routage_Dynamique_Cpp/CMakeLists.txt;0;")
+add_test(PathPlannerTest "C:/Users/takie/Downloads/Projet_Cpp/Projet-Cpp-Sous-Projet-3-Routage Dynamique/Sous_Projet_3_Routage_Dynamique_Cpp/build/test_PathPlanner.exe")
+set_tests_properties(PathPlannerTest PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/takie/Downloads/Projet_Cpp/Projet-Cpp-Sous-Projet-3-Routage Dynamique/Sous_Projet_3_Routage_Dynamique_Cpp/CMakeLists.txt;214;add_test;C:/Users/takie/Downloads/Projet_Cpp/Projet-Cpp-Sous-Projet-3-Routage Dynamique/Sous_Projet_3_Routage_Dynamique_Cpp/CMakeLists.txt;0;")
+add_test(RouteTest "C:/Users/takie/Downloads/Projet_Cpp/Projet-Cpp-Sous-Projet-3-Routage Dynamique/Sous_Projet_3_Routage_Dynamique_Cpp/build/test_Route.exe")
+set_tests_properties(RouteTest PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/takie/Downloads/Projet_Cpp/Projet-Cpp-Sous-Projet-3-Routage Dynamique/Sous_Projet_3_Routage_Dynamique_Cpp/CMakeLists.txt;215;add_test;C:/Users/takie/Downloads/Projet_Cpp/Projet-Cpp-Sous-Projet-3-Routage Dynamique/Sous_Projet_3_Routage_Dynamique_Cpp/CMakeLists.txt;0;")
+add_test(VehicleTest "C:/Users/takie/Downloads/Projet_Cpp/Projet-Cpp-Sous-Projet-3-Routage Dynamique/Sous_Projet_3_Routage_Dynamique_Cpp/build/test_Vehicle.exe")
+set_tests_properties(VehicleTest PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/takie/Downloads/Projet_Cpp/Projet-Cpp-Sous-Projet-3-Routage Dynamique/Sous_Projet_3_Routage_Dynamique_Cpp/CMakeLists.txt;216;add_test;C:/Users/takie/Downloads/Projet_Cpp/Projet-Cpp-Sous-Projet-3-Routage Dynamique/Sous_Projet_3_Routage_Dynamique_Cpp/CMakeLists.txt;0;")
